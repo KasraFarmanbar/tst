a353d0e59ab6a3c934058d55eb7a89da9bc7c4d8
KasraFarmanbar <kfarmanbar7@gmail.com>
2026-06-24 13:20:46 +0330

    Add sound and playerprefs

--- Assets/Scripts/KillEnemy.cs
+++ Assets/Scripts/KillEnemy.cs
@@ -1,16 +1,17 @@
 
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class KillEnemy : MonoBehaviour
 {
-   
-
+    
     private Text scoreinfo;
     private static int score;
 
+
     private void Start()
-    {
+    {   Load();
         scoreinfo = GameObject.Find("scoreinfi").GetComponent<Text>();
     }
 
@@ -18,10 +19,22 @@ public class KillEnemy : MonoBehaviour
     {
         if (other.CompareTag("bullet"))
         {
+            GameObject.Find("Boom").GetComponent<AudioSource>().Play();
             Destroy(this.gameObject);
             Destroy(other.gameObject);
             score++;
             scoreinfo.text = "Score: " + score;
+            Save();
         }
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetInt("score", score);
+    }
+
+    private void Load()
+    {
+        score=PlayerPrefs.GetInt("score");
     }
 }
